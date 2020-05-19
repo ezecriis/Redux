5cdeaff0f589eb0e685cca8c79cead505b507c99
Cris <cr.ledesma@hotmail.com>
2020-05-19 11:38:56 -0300

    Added reducers multiple

---
+++ src/actions/publicacionesActions.js
@@ -0,0 +1,22 @@
+import Axios from "axios"
+import { TRAER_TODOS, CARGANDO, ERROR } from '../types/publicacionesTypes';
+
+export const traerTodos = () => async (dispatch) => {
+    dispatch({
+        type: CARGANDO
+    });
+    try {
+        const respuesta = await Axios.get('http://jsonplaceholder.typicode.com/posts');
+        dispatch({
+            type: TRAER_TODOS,
+            payload: respuesta.data
+        });
+    }
+    catch (error) {
+        console.log('Error: ', error.message);
+        dispatch({
+            type: ERROR,
+            payload: 'Algo salio mal, intente mas tarde.'
+        })
+    }
+}
--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as usuariosActions from '../../actions/usuariosActons';
+import * as publicacionesActions from '../../actions/publicacionesActions';
+
+const { traerTodos: usuariosTraerTodos } = usuariosActions;
+const { traerTodos: publicacionesTraerTodos } = publicacionesActions;
 
 class Publicaciones extends Component {
+    componentDidMount() {
+        if (!this.props.usuariosReducer.usuarios.length) {
+            this.props.usuariosTraerTodos();
+        }
+    }
+
     render() {
+        console.log(this.props);
         return (
-            <div> {this.props.match.params.key} </div>
+            <div><h1>Publicaciones de</h1>
+                {this.props.match.params.key}
+            </div>
         )
     }
 }
 
-export default Publicaciones;
+const mapStateToProps = ({usuariosReducer, publicacionesReducer}) => {
+    return {
+        usuariosReducer,
+        publicacionesReducer
+    };
+};
+
+const mapDispatchToProps = {
+    usuariosTraerTodos,
+    publicacionesTraerTodos
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones);
